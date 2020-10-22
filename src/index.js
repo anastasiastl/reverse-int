0b871f7bab8c8265714032f5b8d350f71cc96352
Anastasia Tihon <stalchinskaya@gmail.com>
2020-10-22 22:05:42 +0200

    feat: solve

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    var string = String(Math.abs(n));
+    var splitString = string.split('');
+    var reverseString = splitString.reverse();
+    var resultString = reverseString.join('');
+    return Number(resultString);
 }
